41a68f4cf42c05e2ece54b53c7ef304e510aaef0
Rafael da Conceição <mnraffaell@gmail.com>
2025-08-25 10:39:10 -0300

    roda pelo amor de Deus

--- src/app/(simulation-page)/simular/(landing-page)/page.tsx
+++ src/app/(simulation-page)/simular/(landing-page)/page.tsx
@@ -1,50 +1,14 @@
-import dynamic from 'next/dynamic';
-
-const SimulationPageHeader = dynamic(() => import(
-  './_components/header'
-).then(mod => mod.SimulationPageHeader), { ssr: true });
-
-const SimulationPageExclusiveBenefitsSection = dynamic(() => import(
-  './_components/exclusive-benefit-section'
-).then(mod => mod.SimulationPageExclusiveBenefitsSection), { ssr: true });
-
-const SimulationPageCompanyStatsSection = dynamic(() => import(
-  './_components/company-stats-section'
-).then(mod => mod.SimulationPageCompanyStatsSection), { ssr: true });
-
-const SimulationPageLoanProcessSection = dynamic(() => import(
-  './_components/loan-process-section'
-).then(mod => mod.SimulationPageLoanProcessSection), { ssr: true });
-
-const SimulationPageCompanyMissionSection = dynamic(() => import(
-  './_components/company-mission-section'
-).then(mod => mod.SimulationPageCompanyMissionSection));
-
-const SimulationPageLGPDSection = dynamic(() => import(
-  './_components/lgpd-section'
-).then(mod => mod.SimulationPageLGPDSection), { ssr: true });
-
-const SimulationPageInfiniteSliderBanksSection = dynamic(() => import(
-  './_components/infinite-slider-banks-section'
-).then(mod => mod.SimulationPageInfiniteSliderBanksSection), { ssr: true });
-
-const SimulationPageInfiniteSliderBanksDesktopSection = dynamic(() => import(
-  './_components/infinite-slider-banks-desktop-section'
-).then(mod => mod.SimulationPageInfiniteSliderBanksDesktopSection), { ssr: true });
-
-const SimulationPageHeroVideoSection = dynamic(() => import(
-  './_components/hero-video-section'
-).then(mod => mod.SimulationPageHeroVideoSection), { ssr: true });
-
-const SimulationPageReviewsSection = dynamic(() => import(
-  './_components/reviews-section'
-).then(mod => mod.SimulationPageReviewsSection), { ssr: true });
-
-const SimulationPageFooter = dynamic(() => import(
-  './_components/footer'
-).then(mod => mod.SimulationPageFooter), { ssr: true });
-
-export const revalidate = 21600 // 6 horas em segundos
+import { SimulationPageCompanyMissionSection } from "./_components/company-mission-section";
+import { SimulationPageCompanyStatsSection } from "./_components/company-stats-section";
+import { SimulationPageExclusiveBenefitsSection } from "./_components/exclusive-benefit-section";
+import { SimulationPageFooter } from "./_components/footer";
+import { SimulationPageHeader } from "./_components/header";
+import { SimulationPageHeroVideoSection } from "./_components/hero-video-section";
+import { SimulationPageInfiniteSliderBanksDesktopSection } from "./_components/infinite-slider-banks-desktop-section";
+import { SimulationPageInfiniteSliderBanksSection } from "./_components/infinite-slider-banks-section";
+import { SimulationPageLGPDSection } from "./_components/lgpd-section";
+import { SimulationPageLoanProcessSection } from "./_components/loan-process-section";
+import { SimulationPageReviewsSection } from "./_components/reviews-section";
 
 export default function SimulationLandingPage() {
   return (
