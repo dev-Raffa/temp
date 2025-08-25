a7e778df068aef18f16b3b04c0f66f0b304a1220
Rafael da Conceição <mnraffaell@gmail.com>
2025-08-25 10:25:17 -0300

    foooi

--- src/app/api/brasilcard/(routers)/client-analyse/route.ts
+++ src/app/api/brasilcard/(routers)/client-analyse/route.ts
@@ -4,45 +4,48 @@ import { AnalysisForm } from "@/app/(partners)/brasilcard/_components/forms/anal
 import { loginBrasilcard } from "../../functions/login-brasilcard";
 import { preApprovalBrasilcard } from "../../functions/pre-approval-brasilcard";
 import { clientAnalyseBrasilcard } from "../../functions/client-analyse";
+import { ClientAnalyseResponse } from "../../types/customer-analyse";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     
-    if(body){
-      const client: AnalysisForm = body
-
-      client.birth_date = client.birth_date.split("/").reverse().join("-")
-      client.cpf = client.cpf.replaceAll(".",'').replaceAll("-","")
-            
-      const login = await loginBrasilcard({
-        relative: env.NEXT_PUBLIC_BRASIL_CARD_RELATIVE,
-        username: env.NEXT_PUBLIC_BRASIL_CARD_USERNAME,
-        password: env.NEXT_PUBLIC_BRASIL_CARD_PASSWORD,
-        captcha: env.NEXT_PUBLIC_BRASIL_CARD_CAPTCHA
-      })
-
-      if(!login.access_token){
-        return NextResponse.json({ error: "Erro ao autenticar na Brasilcard" }, {status: 401} );
-      }
-
-      const preAnalyseResponse = await preApprovalBrasilcard(client, login.access_token)
-
-      if(preAnalyseResponse.released_value <= 0){
-        return NextResponse.json(
-          {
-            pre_approved: false,
-            card_limit: 0,
-            status: "rejected",
-            message: "Cliente não foi aprovado"
-          }
-        );
-      }
-      
-      const analysisReponse = await clientAnalyseBrasilcard(client, login.access_token)
-      
-      return NextResponse.json(analysisReponse);
+    if (!body) {
+      return NextResponse.json({ error: "Corpo da requisição ausente" }, { status: 400 });
     }
+
+    const client: AnalysisForm = body
+
+    client.birth_date = client.birth_date.split("/").reverse().join("-")
+    client.cpf = client.cpf.replaceAll(".",'').replaceAll("-","")
+          
+    const login = await loginBrasilcard({
+      relative: env.NEXT_PUBLIC_BRASIL_CARD_RELATIVE,
+      username: env.NEXT_PUBLIC_BRASIL_CARD_USERNAME,
+      password: env.NEXT_PUBLIC_BRASIL_CARD_PASSWORD,
+      captcha: env.NEXT_PUBLIC_BRASIL_CARD_CAPTCHA
+    })
+
+    if(!login.access_token){
+      return NextResponse.json({ error: "Erro ao autenticar na Brasilcard" }, {status: 401} );
+    }
+
+    const preAnalyseResponse = await preApprovalBrasilcard(client, login.access_token)
+
+    if(preAnalyseResponse.released_value <= 0){
+      const response: ClientAnalyseResponse = {
+          pre_approved: false,
+          card_limit: 0,
+          status: "rejected",
+          message: "Cliente não foi aprovado"
+        }
+
+      return NextResponse.json(response);
+    } 
+      
+    const analysisReponse = await clientAnalyseBrasilcard(client, login.access_token)
+      
+    return NextResponse.json(analysisReponse);
     
   } catch (error) {
     return NextResponse.json({ error }, {status: 500} );
